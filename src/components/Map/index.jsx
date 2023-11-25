ecb446d7c017ab802984f297cd1cf81b6d3bdd4c
z297xu <kyrie807782352@gmail.com>
2023-11-25 17:51:33 -0600

    Displaying City Markers on Map

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,16 +1,16 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from 'leaflet';
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-
+import L from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 import styles from "./index.module.css";
 import { useState } from "react";
+import { useCities } from "../../contexts/CityContext";
 
 let DefaultIcon = L.icon({
   iconUrl: icon,
-  shadowUrl: iconShadow
+  shadowUrl: iconShadow,
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
@@ -20,23 +20,34 @@ export default function Map() {
 
   const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
 
+  const { cities } = useCities();
+
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
-  const navigate = useNavigate();
-
   return (
     <div className={styles.mapContainer}>
-      <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <img src={city.imgUrl} alt={city.country} />
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
