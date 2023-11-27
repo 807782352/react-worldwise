2d82da6c416ea4d80e798e4e478901f8f1747f56
z297xu <kyrie807782352@gmail.com>
2023-11-26 20:30:43 -0500

    update useState to useReducer in CityContext

--- src/contexts/CityContext.jsx
+++ src/contexts/CityContext.jsx
@@ -1,29 +1,85 @@
 // All cities operations are written here ...
-import { createContext, useContext, useEffect, useState } from "react";
-
 import axios from "axios";
+import { createContext, useContext, useEffect, useReducer } from "react";
 
 const BASE_URL = "http://localhost:9000";
 
 const CityContext = createContext();
 
+const initState = {
+  cities: [],
+  isLoading: false,
+  currentCity: {},
+  error: "",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case "cities/loaded":
+      return {
+        ...state,
+        isLoading: false,
+        cities: action.payload,
+      };
+    case "city/loaded":
+      return {
+        ...state,
+        isLoading: false,
+        currentCity: action.payload,
+      };
+    case "city/created":
+      return {
+        ...state,
+        cities: [...state.cities, action.payload],
+        isLoading: false,
+        currentCity: action.payload,
+      };
+
+    case "city/deleted":
+      return {
+        ...state,
+        isLoading: false,
+        cities: state.cities.filter((city) => city.id !== action.payload),
+        currentCity: {},
+      };
+
+    case "rejected":
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+
+    default:
+      throw new Error("Unknown action type");
+  }
+}
+
 function CityProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
+  const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(
+    reducer,
+    initState
+  );
 
   // empty space, only render once during the init
   useEffect(function () {
     async function fetchCities() {
+      dispatch({ type: "loading" });
+
       try {
-        setIsLoading(true);
         const res = await fetch(`${BASE_URL}/cities`);
         const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("There was an error loading data");
-      } finally {
-        setIsLoading(false);
+        dispatch({ type: "cities/loaded", payload: data });
+      } catch (error) {
+        dispatch({
+          type: "rejected",
+          payload: "There was an error loading cities...",
+        });
       }
     }
 
@@ -31,64 +87,52 @@ function CityProvider({ children }) {
   }, []);
 
   async function getCity(id) {
+
+    if (Number(id) === currentCity.id) return;
+
+    dispatch({ type: "loading" });
     try {
-      setIsLoading(true);
       const res = await fetch(`${BASE_URL}/cities/${id}`);
       const data = await res.json();
-      setCurrentCity(data);
+
+      dispatch({ type: "city/loaded", payload: data });
     } catch {
-      alert("There was an error loading data");
-    } finally {
-      setIsLoading(false);
+      dispatch({
+        type: "rejected",
+        payload: "There was an error loading city...",
+      });
     }
   }
 
   async function createCity(newCity) {
+    dispatch({ type: "loading" });
     try {
-      setIsLoading(true);
-
-      // POST request to the server (9000)
-      // const res = await fetch(`${BASE_URL}/cities`, {
-      //   method: "POST",
-      //   body: JSON.stringify(newCity),
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      // });
-
       const res = await axios.post(`${BASE_URL}/cities`, newCity, {
         headers: {
           "Content-Type": "application/json",
         },
       });
 
-      console.log(res.data);
       const data = res.data;
-
-      // sync by rerendering
-      // works on small project, but there is a better way to do it as well
-      setCities((cities) => [...cities, data]);
+      dispatch({ type: "city/created", payload: data });
     } catch {
-      alert("There was an error loading data");
-    } finally {
-      setIsLoading(false);
+      dispatch({
+        type: "rejected",
+        payload: "There was an error creating the city...",
+      });
     }
   }
 
   async function deleteCity(id) {
+    dispatch({ type: "loading" });
     try {
-      setIsLoading(true);
-
-      const res = await axios.delete(`${BASE_URL}/cities/${id}`);
-
-      console.log(res.data);
-      const data = res.data;
-
-      setCities((cities) => cities.filter((city) => city.id !== id));
+      await axios.delete(`${BASE_URL}/cities/${id}`);
+      dispatch({ type: "city/deleted", payload: id });
     } catch {
-      alert("There was an error loading data");
-    } finally {
-      setIsLoading(false);
+      dispatch({
+        type: "rejected",
+        payload: "There was an error deleting the city...",
+      });
     }
   }
 
@@ -98,6 +142,7 @@ function CityProvider({ children }) {
         cities,
         isLoading,
         currentCity,
+        error,
         getCity,
         createCity,
         deleteCity,
---
+++ src/contexts/CityContext_useState.jsx
@@ -0,0 +1,122 @@
+// All cities operations are written here ...
+import { createContext, useContext, useEffect, useState } from "react";
+
+import axios from "axios";
+
+const BASE_URL = "http://localhost:9000";
+
+const CityContext = createContext();
+
+function CityProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
+
+  // empty space, only render once during the init
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert("There was an error loading data");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true);
+
+      // POST request to the server (9000)
+      // const res = await fetch(`${BASE_URL}/cities`, {
+      //   method: "POST",
+      //   body: JSON.stringify(newCity),
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      // });
+
+      const res = await axios.post(`${BASE_URL}/cities`, newCity, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      console.log(res.data);
+      const data = res.data;
+
+      // sync by rerendering
+      // works on small project, but there is a better way to do it as well
+      setCities((cities) => [...cities, data]);
+    } catch {
+      alert("There was an error loading data");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+
+      const res = await axios.delete(`${BASE_URL}/cities/${id}`);
+
+      console.log(res.data);
+      const data = res.data;
+      // data: {}
+
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch {
+      alert("There was an error loading data");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <CityContext.Provider
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
+    >
+      {children}
+    </CityContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CityContext);
+
+  if (context === undefined) {
+    throw new Error("CityContext is used outside of the CityProvider");
+  }
+
+  return context;
+}
+
+export { CityProvider, useCities };
